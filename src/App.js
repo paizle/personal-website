3340c5496b0d1237e071cc0848c4734efd7e6bfb
paizle <44871665+paizle@users.noreply.github.com>
2023-08-28 09:39:23 -0300

    Created project skelleton.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.scss'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="app-header">
+                <div className="banner">
+                    <h1>Matthew MacPherson</h1>
+                </div>
+                <div className="menu">
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                    </ul>
+                </div>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
